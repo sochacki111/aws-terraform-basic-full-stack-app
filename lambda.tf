8b07391052b8e4f3e0b628effaef46a37b3f8d32
Michał Sochacki <michalsochacki111@gmail.com>
2024-07-13 11:44:08 +0200

    feat: s3 hosting website

--- lambda.tf
+++ lambda.tf
@@ -5,13 +5,12 @@ data "archive_file" "lambda_zip" {
 }
 
 resource "aws_lambda_function" "my_lambda_function" {
-  memory_size   = "256"
-  function_name = "my_lambda_function"
-  role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  filename      = data.archive_file.lambda_zip.output_path
-
+  memory_size      = "256"
+  function_name    = "my_lambda_function"
+  role             = aws_iam_role.lambda_exec_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   environment {
@@ -19,4 +18,14 @@ resource "aws_lambda_function" "my_lambda_function" {
       foo = "bar"
     }
   }
+}
+
+resource "aws_lambda_function_url" "my_lambda_function_url" {
+  function_name      = aws_lambda_function.my_lambda_function.function_name
+  authorization_type = "NONE"
+
+}
+
+output "my_lambda_function_url" {
+  value = aws_lambda_function_url.my_lambda_function_url.function_url
 }
---
+++ s3.tf
@@ -0,0 +1,67 @@
+resource "aws_s3_bucket" "public_fortune_bucket" {
+  bucket_prefix = "public-fortune-bucket-"
+  tags = {
+    Name        = "MyPublicBucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "fortune_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.public_fortune_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_fortune_bucket_policy" {
+  bucket = aws_s3_bucket.public_fortune_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.public_fortune_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.public_fortune_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.public_fortune_bucket.bucket
+  key          = "index.html"
+  source       = "./resources/s3/index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" {
+  bucket       = aws_s3_bucket.public_fortune_bucket.bucket
+  key          = "error.html"
+  source       = "./resources/s3/error.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "cookieJpg" {
+  bucket = aws_s3_bucket.public_fortune_bucket.bucket
+  key    = "cookie.jpg"
+  source = "./resources/s3/cookie.jpg"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.public_fortune_bucket.bucket
+}
